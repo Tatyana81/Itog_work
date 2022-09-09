1df49279181604f1f827d1f0758a2d412b8c29d0
«TanchaTi» <tatyanamokrushina@yandex.ru>
2022-09-09 13:36:59 +0700

    создала метод и добавила описание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+//Примеры:
+//["Hello", "2", "world", ":-)"] → [“2”, “:-)”]
+//["1234", "1567", "-2", "computer science"] → [“-2”]
+//[“Russia”, “Denmark”, “Kazan”] → []
+
+string[]cc = {"Russia", "Denmark", "Kazan"};
+int count=0;
+int j=0;
+string s;
+
+for(int i=0;i<cc.Length;i++) //находим длину нового массива
+    {
+        Metod(cc[i]);
+        if (s.Length<=3) 
+            {
+                count++;
+            }
+        
+    }
+if (count==0)
+    {
+        System.Console.WriteLine("нет элементов удовлетворяющих условию задачи");
+        return;
+    }
+
+      
+      
+void Metod (string a)//массив из элементов определенного элемента массива строк
+    { 
+        s=a;
+        char[] array=s.ToArray();
+        s=new String(array);
+    }
+
+
